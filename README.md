28a73fe3fe9a7b5ad0e31977867d2017d02fcfdc
Jeremy Roberts <jaroberts@ksu.edu>
2020-11-11 15:04:17 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# README.md
+
+This repository contains several demo programs
+in `bash`, `python`, and `c++` for the 
+raspberry pi.
---
+++ sleep_loop.cc
@@ -0,0 +1,25 @@
+#include <unistd.h> // POSIX Api
+#include <iostream>
+#include <string>
+#include <sstream>
+
+int main(int argc, char* argv[])
+{
+    std::string msg = "Hello, world!";
+    int num_repeats = 10; // repeats
+    int num_seconds = 1 ; // seconds 
+    if (argc == 4)
+    {
+        msg = argv[1];
+        std::string buffer(argv[2]);
+        std::stringstream(buffer) >> num_repeats;
+        buffer= argv[3];
+        std::stringstream(buffer) >> num_seconds;
+    }
+    for (int i = 0; i < num_repeats; ++i)
+    {
+        std::cout << msg << std::endl;
+        sleep(num_seconds);
+    }
+    return 0;
+}
